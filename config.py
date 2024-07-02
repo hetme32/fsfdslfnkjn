e27e990d0295702a26af26ca221e407976923780
mdrobot3115 <116418622+mdrobot3115@users.noreply.github.com>
2024-07-02 18:57:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-API_ID = int(os.environ.get("API_ID", "523894"))
-API_HASH = os.environ.get("API_HASH", "e15ad5314295f")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "683894700450:AAGtmIuSJgmjbkbi3vs")
+API_ID = int(os.environ.get("API_ID", ""))
+API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>{file_name}</b>\n\n<b>JOIN 💎 : @M2LINKS</b>")
