e379d1ccbed9f8e05de71f167a0a8dcf0d5a5e0d
Linbu Soda <128895838+LimbuSoda@users.noreply.github.com>
2024-07-06 15:07:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 API_ID = int(os.environ.get("API_ID", ""))
 API_HASH = os.environ.get("API_HASH", "")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>{file_name}</b>\n<i>#𝙋𝙧𝙞𝙢𝙚 – #𝙉𝙤𝟏 Biggest Hording Telegram Premium Channel</i>\n⏤͟͞𝗝⌡𝗼𝗶𝗻 𝗣𝗿𝗲𝗺𝗶𝘂𝗺 <b>–「 @PrimeFix 」</b>")
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>{file_caption}</b>\n<i>#𝙋𝙧𝙞𝙢𝙚 – #𝙉𝙤𝟏 Biggest Hording Telegram Premium Channel</i>\n⏤͟͞𝗝⌡𝗼𝗶𝗻 𝗣𝗿𝗲𝗺𝗶𝘂𝗺 <b>–「 @PrimeFix 」</b>")
